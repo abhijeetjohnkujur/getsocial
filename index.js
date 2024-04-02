77437073eb7b84b71f9bced05983387aaecbf762
abhijeetjohnkujur <abhijeetkujur000@gmail.com>
2024-04-02 11:57:59 +0530

    Added files and csspath tweak

--- index.js
+++ index.js
@@ -4,21 +4,28 @@ const app = express();
 const port = 3000;
 const expressLayouts = require('express-ejs-layouts');
 
+// add layout middleware
 app.use(expressLayouts);
 
+//add body parser
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
+// add middleware for accessing static files
 app.use(express.static('assets'));
 
 
+// Add view engine
 app.set('view engine', 'ejs');
 app.set('views','./views');
 
 
+// Adding parent route folder using middlware
 app.use('/', require('./routes/index'));
 
+// Adding child route folder using middlware
+app.use('/users', require('./routes/users'));
 
-
+// Server start script
 app.listen(port, (err) => err ? console.log(err) : console.log(`Server is running on port ${port}`));
