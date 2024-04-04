6b0d5f05aaaf15eb91b3c97fb24f3874e225c3ca
abhijeetjohnkujur <abhijeetkujur000@gmail.com>
2024-04-04 16:17:09 +0530

    Added user schema

---
+++ controllers/userController.js
@@ -0,0 +1,5 @@
+module.exports.signup = (req,res) => {
+    return res.render('signup',{
+        title: "Sign Up Page"
+    })
+}
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const profileController = require('../controllers/profileController');
+const userController = require('../controllers/userController');
 
 
 router.get('/profile', profileController.profile)
+
+
+router.get('/signup',userController.signup)
 
 module.exports = router
